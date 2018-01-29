67ce50e4373e09007b97fe63819e8e416a647e08
madrury <matthew.drury.83@gmail.com>
2018-01-29 10:15:19 -0800

    Add docstrings to example classes.

--- week4/day7-intro_to_classes/matt_drury/stringstamper.py
+++ week4/day7-intro_to_classes/matt_drury/stringstamper.py
@@ -1,6 +1,13 @@
 class StringStamper:
     """Stamps a message on the end of a string.
 
+    Attributes
+    ----------
+    message: str
+      A message to stamp on the end of a string.
+
+    Usage
+    -----
     $ stamper = StringStamper("Property of Matt.")
     $ stamper.stamp("Elements of Statistical Learning.")
     "Elements of Statistical Learning. Property of Matt."
