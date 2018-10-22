989de7b177c974bcfb46fa7ecfb1397d633306a7
Will Bryant <william.steen.bryant@gmail.com>
2018-10-22 13:07:01 -0600

    adding lists work

--- week2/day3-beyond_numerics/warmups/warmup1.py
+++ week2/day3-beyond_numerics/warmups/warmup1.py
@@ -1,2 +1,10 @@
-my_str = 'This String was not Chosen Arbitrarily...'
+my_str = 'This String wasn\'t Chosen Arbitrarily...'
+next_str = "oR was it?"
 print(my_str.upper())
+# i
+print(my_str[14])
+#ii
+
+# iii
+print(my_str.lower()
+# iv
